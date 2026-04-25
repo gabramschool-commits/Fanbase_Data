e6a6e6b31e9158972257adafd909199710822d5a
Gabriel Mar Ramirez <gabramschool@gmail.com>
2026-04-25 18:37:19 +0800

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.routers import Arcane_characters
+from app.routers import Arcane_voice_actors
+from contextlib import asynccontextmanager
+from app.database import create_db_and_tables, engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(Arcane_voice_actors.router)
+app.include_router(Arcane_characters.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "HELLO HUMAN, IM A FISH"}
